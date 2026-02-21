ea2df9bcc8dba8212032eb959efea7e2e3c545f3
MarcheseLeo <leonardo.lol.ldp@gmail.com>
2026-02-21 15:40:23 +0100

    updating accordions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-const btn = document.querySelector('.btn-accordion')
-console.log(btn.getAttribute("btn-target"))
+const btnList = document.querySelectorAll('.btn-accordion')
 
-btn.addEventListener('click', () =>{
-    const element = document.querySelector(btn.getAttribute("btn-target"))
-    element.classList.toggle('collapse')
-    btn.lastChild.classList.toggle('fa-chevron-down')
-    btn.lastChild.classList.toggle('fa-chevron-up')
-
-
+btnList.forEach(btn =>{
+    accordion(btn)
+    
 })
+
+function accordion(btn){
+    btn.addEventListener('click', () =>{
+    const element = document.querySelector(btn.getAttribute("btn-target"))
+    let query = "#" + btn.id + ' i'
+    const icon = document.querySelector(query)   
+
+    element.classList.toggle('collapse')
+    icon.classList.toggle('fa-chevron-down')
+    icon.classList.toggle('fa-chevron-up')
+})
+}
